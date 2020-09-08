5674b33da0639e0f738d04566bee2fd18e78ed54
Austin Henderson <46322823+austinhenderson19@users.noreply.github.com>
2020-09-08 19:15:06 -0400

    Added CRUD based operations to the User Service (#5)
    * Added all CRUD operations to the Users service
    
    * Refeactored both GET requests and the POST request
    
    * Refactored the PUT and DELETE User service methods

--- services/users/controllers/user.controller.js
+++ services/users/controllers/user.controller.js
@@ -1,22 +1,147 @@
 const User = require('../models/user.model');
 
+/**
+ * Fetches all User resources.
+ *
+ * @param {*} request
+ * @param {*} response
+ * @param {*} next
+ */
 exports.getUserResources = async (request, response, next) => {
+  let users;
+  let errors;
+
   try {
-    let users = await User.find();
-    response.status(200).json({
-      status: true,
-      message: 'OK',
-      data: users,
-    });
+    users = await User.find();
   } catch (error) {
-    response.status(300).json({
-      status: false,
-      message: 'NOT OK',
-      error,
+    error.statusCode = 500;
+    errors = error;
+  } finally {
+    response.status(errors ? errors.statusCode : 200).json({
+      success: errors ? false : true,
+      data: users ? users : undefined,
+      error: errors ? errors.message : undefined,
     });
   }
 };
-exports.createUserResource = async (request, response, next) => {};
-exports.getUserResourceById = async (request, response, next) => {};
-exports.updateUserResource = async (request, response, next) => {};
-exports.deleteUserResource = async (request, response, next) => {};
+
+/**
+ * Fetches a User resource based on the corresponding id.
+ *
+ * @param {*} request
+ * @param {*} response
+ * @param {*} next
+ */
+exports.getUserResourceById = async (request, response, next) => {
+  let user;
+  let errors;
+
+  try {
+    user = await User.findById(request.params.id);
+
+    if (!user) {
+      errors = { statusCode: 404, message: 'User not found!' };
+    }
+  } catch (error) {
+    error.statusCode = 500;
+    errors = error;
+  } finally {
+    response.status(errors ? errors.statusCode : 200).json({
+      success: errors ? false : true,
+      data: user ? user : undefined,
+      error: errors ? errors.message : undefined,
+    });
+  }
+};
+
+/**
+ * Creates a new User resource.
+ *
+ * @param {*} request
+ * @param {*} response
+ * @param {*} next
+ */
+exports.createUserResource = async (request, response, next) => {
+  let user;
+  let errors;
+
+  try {
+    let createdUser = new User({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      password: request.body.password,
+    });
+    user = await createdUser.save();
+  } catch (error) {
+    error.statusCode = error._message === 'User validation failed' ? 400 : 500;
+    errors = error;
+  } finally {
+    response.status(errors ? errors.statusCode : 201).json({
+      success: errors ? false : true,
+      data: user ? user : undefined,
+      error: errors ? errors.message : undefined,
+    });
+  }
+};
+
+/**
+ * Updated a User resource based on the corresponding id.
+ *
+ * @param {*} request
+ * @param {*} response
+ * @param {*} next
+ */
+exports.updateUserResource = async (request, response, next) => {
+  let user;
+  let errors;
+
+  try {
+    await User.updateOne(
+      { _id: request.params.id },
+      {
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        email: request.body.email,
+        password: request.body.password,
+      }
+    );
+    user = await User.findById(request.params.id);
+
+    if (!user) {
+      errors = { statusCode: 404, message: 'User not found!' };
+    }
+  } catch (error) {
+    error.statusCode = error._message === 'User validation failed' ? 400 : 500;
+    errors = error;
+  } finally {
+    response.status(errors ? errors.status : 200).json({
+      success: errors ? false : true,
+      data: user ? user : undefined,
+      error: errors ? error.message : undefined,
+    });
+  }
+};
+
+/**
+ * Deletes a User resource based on the corresponding id.
+ *
+ * @param {*} request
+ * @param {*} response
+ * @param {*} next
+ */
+exports.deleteUserResource = async (request, response, next) => {
+  let errors;
+
+  try {
+    await User.deleteOne({ _id: request.params.id });
+  } catch (error) {
+    error.statusCode = 500;
+    errors = error;
+  } finally {
+    response.status(errors ? errors.statusCode : 200).json({
+      success: errors ? false : true,
+      error: errors ? errors.message : undefined,
+    });
+  }
+};
